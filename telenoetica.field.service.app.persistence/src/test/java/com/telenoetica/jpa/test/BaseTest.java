0d8f80ff95658454ae59586ece8063bcbe9e7b29
shivprasadk <shivprasad.khillar@citrix.com>
2013-04-21 03:11:01 +0530

    added property file integration

--- telenoetica.field.service.app.persistence/src/test/java/com/telenoetica/jpa/test/BaseTest.java
+++ telenoetica.field.service.app.persistence/src/test/java/com/telenoetica/jpa/test/BaseTest.java
@@ -6,7 +6,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-		"classpath:applicationContext-data-source-test.xml",
+    "classpath:applicationContext-property-initializer-test.xml",
+    "classpath:applicationContext-data-source.xml",
 		"classpath:applicationContext-liquibase.xml",
 		"classpath:spring-data-jpa-config.xml" })
 public class BaseTest {
