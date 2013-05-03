72d17104703e38f2c70b1e299f636334d0b85861
shivprasadk <shivprasad.khillar@citrix.com>
2013-05-04 01:42:58 +0530

    Quartz Job Scheduler setup.

--- telenoetica.field.service.app.scheduler/src/main/java/com/telenoetica/scheduler/job/CallOutVisitJob.java
+++ telenoetica.field.service.app.scheduler/src/main/java/com/telenoetica/scheduler/job/CallOutVisitJob.java
@@ -1,14 +1,29 @@
 package com.telenoetica.scheduler.job;
 
+import org.apache.log4j.Logger;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import com.telenoetica.service.CallOutVisitService;
+
 public class CallOutVisitJob extends QuartzJobBean {
+  
+  private static final Logger LOGGER = Logger.getLogger(CallOutVisitJob.class);
+  
+  private CallOutVisitService callOutVisitService;
+
+  public CallOutVisitService getCallOutVisitService() {
+    return callOutVisitService;
+  }
+
+  public void setCallOutVisitService(CallOutVisitService callOutVisitService) {
+    this.callOutVisitService = callOutVisitService;
+  }
 
   @Override
   protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-    
+    LOGGER.debug("CallOutVisitJob. started..");
 
   }
 
